f411e85ff36e119026c01938073fd57e40c416ab
Shen <863461783@qq.com>
2021-04-27 18:50:50 +0800

    0441

--- binary_search/0441_arrange_coins.js
+++ binary_search/0441_arrange_coins.js
@@ -1,10 +1,10 @@
-const sqrt = (n) => {
+const arrangeCoins = (n) => {
     let start = 0;
     let end = n;
     while (start < end) {
         let mid = Math.floor(start + (end - start) / 2);
-        if (mid ** 2 < n) {
-            if ((mid + 1) ** 2 > n) {
+        if ((mid + 1) * mid / 2 < n) {
+            if ((mid + 1 + 1) * (mid + 1) / 2 > n) {
                 return mid;
             }
             start = mid + 1;
@@ -13,6 +13,6 @@ const sqrt = (n) => {
         }
     }
     return start;
-}
+};
 
-export default sqrt;
+export default arrangeCoins;
---
+++ binary_search/0441_arrange_coins.test.js
@@ -0,0 +1,9 @@
+import arrangeCoins from './0441_arrange_coins';
+
+test('sqrt:1', () => {
+    expect(arrangeCoins(5)).toBe(2);
+});
+
+test('sqrt:2', () => {
+    expect(arrangeCoins(8)).toBe(3);
+});
