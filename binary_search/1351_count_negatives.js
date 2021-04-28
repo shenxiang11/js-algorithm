cd2b562e608f28ade3fd6e45f817589c9192c52c
Shen <863461783@qq.com>
2021-04-28 11:34:23 +0800

    comment

--- binary_search/1351_count_negatives.js
+++ binary_search/1351_count_negatives.js
@@ -4,7 +4,7 @@ const countNegatives = function(grid) {
     }
     let count = 0;
     for (let i = 0; i < grid.length; i++) {
-        let idx = grid[i].findIndex(i => i < 0); // 二分查找找到第一个负数
+        let idx = grid[i].findIndex(i => i < 0); // 二分查找找到第一个负数，转化单调递减的数组找第一个负数的问题
         if (idx != -1) {
             count += grid[i].length - idx;
         }
