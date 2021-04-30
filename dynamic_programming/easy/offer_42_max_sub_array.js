e7be3a7edaa850184f91bd96274e43707c1a291e
Shen <863461783@qq.com>
2021-04-30 09:24:01 +0800

    offer 42

---
+++ dynamic_programming/easy/offer_42_max_sub_array.js
@@ -0,0 +1,11 @@
+const maxSubArray = function(nums) {
+    let dp = nums[0];
+    let max = nums[0];
+    for (let i = 1, len = nums.length; i < len; i++) {
+        dp = Math.max(dp + nums[i], nums[i]);
+        max = Math.max(dp, max);
+    }
+    return max;
+};
+
+export default maxSubArray;
