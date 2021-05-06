b8bde012dc477f8a4e65499eb57c0e7eb27733ae
Shen <863461783@qq.com>
2021-05-06 09:46:58 +0800

    better 0001

--- array/easy/0001_two_sum.js
+++ array/easy/0001_two_sum.js
@@ -1,9 +1,10 @@
 const twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+    const m = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (m.has(nums[i])) {
+            return [m.get(nums[i]), i];
+        } else {
+            m.set(target - nums[i], i);
         }
     }
 };
